a0f418715742b376c96dac6f8ec60c17d1c94358
Stamplin <701017313@gotvdsb.ca>
2024-09-10 09:11:50 -0400

    Program.cs

--- project2/Program.cs
+++ project2/Program.cs
@@ -1,26 +1,18 @@
-﻿using System.Dynamic;
-//Numbers and Math lesson
-namespace project2
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("1) How much money would you make if you were to work for 8 hours while earning $21/hour?");
+//Task 1
+//S.B
 
-            Console.WriteLine((8 * 21).ToString("C"));
-            Console.WriteLine("")
+//Task 2
+Console.WriteLine(2024 - 16);
 
-            Console.WriteLine("2) How much would a dozen (12) eggs cost if one egg is 6$?");
-            Console.WriteLine("$" + (6 * 12));
-            Console.WriteLine("");
-            
-            Console.WriteLine("3) What is the 13% tax of $79.99?");
-            Console.WriteLine("$" + (79.99*0.13));
-            Console.WriteLine("");
+//Task 3
+Console.WriteLine((14+15+16) / 3);
 
-            Console.WriteLine("What is 5'6 in cm?");
-            Console.WriteLine((2.54 * 12 * 5) + (6 * 2.54) + "cm");
-        }
-    }
-}
+//Task 4
+Console.WriteLine((14+15.0+16+17)/4);
+
+//Task 5
+Console.WriteLine((3.14) * (3*3));
+
+//Task 6
+Console.WriteLine("C# knows when to use the '+' operator instead of sticking together (concatenate) as it checks what's before it for example if it's a number + a number it knows that it's a numerical but if there are quotes infront of it, it turns the number infront of it into strings.");
+Console.ReadLine();
